80694140983cdfeea4c5b4a47b1aa788cd81f198
TakaVII <ulirub@gmail.com>
2025-10-24 22:58:44 -0400

    .

---
+++ Globals/character_manager.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var char_ids: Dictionary
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func append_char_id( char_id: int ) -> bool:
+	if char_ids.has(char_id):
+		# Match found, ID is already present.
+		return false
+	else:
+		# No match. Add the ID as a key. 
+		char_ids[char_id] = true
+		return true
---
+++ Scripts/Characters/culture.gd
@@ -0,0 +1,7 @@
+class_name Culture extends Resource
+
+enum Region {EASTERN, WESTERN}
+
+@export var culture_id: int
+@export var culture_name: String
+@export var culture_region: Region
